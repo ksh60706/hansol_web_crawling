c93f4d1cfd2eec7b6ad01c75f87cec5d6e4a2394
SHx3 <ksh60706@gmail.com>
2019-10-29 13:49:10 +0900

    공공데이터포탈 API 중 자외선지수 개발

---
+++ kma/h_crawling_kma_uitrv.py
@@ -0,0 +1,13 @@
+
+TARGET_URL_ENDPOINT="http://newsky2.kma.go.kr/iros/RetrieveLifeIndexService3/getUltrvLifeList?"
+TARGET_URL_SERVICEKEY="ServiceKey==%2FsQMJ9S81k9t5qGUQxLL84cn24R%2Fyl0CcGZfg3%2BJzOlh9PNr0sNlHzV9NjKILYqaC36vg4LW%2BamHp0e49UoKkA%3D%3D"
+TARGET_URL_AREANO="&areaNo=1100000000"  # 서울지점
+TARGET_URL_TYPE="&_type=xml"   # 응답 값 형식 xml, json
+TARGET_URL_TIME="&time="    # 시간값 입력 (YYYYMMDDHH)
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
