660d39415aff19fa8f338eaa51b68fef1453baee
SHx3 <ksh60706@gmail.com>
2019-11-01 14:56:27 +0900

    유가정보 크롤링 파일 생성

---
+++ api/h_crawling_gov_oil.py
@@ -0,0 +1,6 @@
+# 유가정보 API 크롤링
+
+http://api.apistore.co.kr/opinetbasic/Oil/OilPriceAll.jsp?x-waple-authorization=MTE3OTUtMTU3MjM1MzI0OTExOS03YWUxZGY4MS04ZGE4LTQ0OTMtYTFkZi04MThkYTg1NDkzYjg=&OutPut=Json
+
+
+http://api.openapi.io/opinetbasic
