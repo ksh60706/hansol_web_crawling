89f35724c288ef0d7813f704ceac63f8672964de
김수환 <ksh60706@hansol.com>
2019-11-04 14:21:05 +0900

    자외선지수 한글화 추가

--- api/h_crawling_kma_uitrv.py
+++ api/h_crawling_kma_uitrv.py
@@ -50,6 +50,26 @@ def clean_data(content):
     clean_content = content
 
     # 지수에 값이 없는 경우 0 으로 변경, 있으면 숫자형(int)로 변경
+    arr_uitrv = {"uitrvToday", "uitrvTomorrow", "uitrvTheDayAfterTomorrow"}
+
+    for uitrv in arr_uitrv:
+        if content[uitrv] == "":
+            clean_content[uitrv] = 0
+        else:
+            clean_content[uitrv] = int(content[uitrv])
+
+        if clean_content[uitrv] < 3:
+            clean_content[uitrv + "_kor"] = "낮음"
+        elif clean_content[uitrv] >= 3 and clean_content[uitrv] < 6:
+            clean_content[uitrv + "_kor"] = "보통"
+        elif clean_content[uitrv] >= 6 and clean_content[uitrv] < 8:
+            clean_content[uitrv + "_kor"] = "높음"
+        elif clean_content[uitrv] >= 8 and clean_content[uitrv] < 11:
+            clean_content[uitrv + "_kor"] = "매우 높음"
+        else:
+            clean_content[uitrv + "_kor"] = "위험"
+
+    '''
     if content["uitrvToday"] == "":
         clean_content["uitrvToday"] = 0
     else :
@@ -64,6 +84,7 @@ def clean_data(content):
         clean_content["uitrvTheDayAfterTomorrow"] = 0
     else :
         clean_content["uitrvTheDayAfterTomorrow"] = int(content["uitrvTheDayAfterTomorrow"])
+    '''
 
     # Kibana에서는 날짜 형태가 한국 시간이 아닌 (+9시간) 형태라서 변경해줘야함
     clean_content["uitrvDate"] = datetime.strptime(content["uitrvDate"] + "0000", "%Y%m%d%H%M%S") + timedelta(hours=-9)
@@ -82,15 +103,18 @@ def get_content_from_url(URL):
         successYn = note.findtext("Header/SuccessYN")
 
         if successYn == "Y":
-            print("찾기 성공")
+            #print("찾기 성공")
             content = {
                 "apiURL"                   : URL,
                 "uitrvCode"                : note.findtext("Body/IndexModel/code"),
                 "uitrvAreaNo"              : note.findtext("Body/IndexModel/areaNo"),
                 "uitrvDate"                : note.findtext("Body/IndexModel/date"),
                 "uitrvToday"               : note.findtext("Body/IndexModel/today"),
+                "uitrvToday_kor"           : "",
                 "uitrvTomorrow"            : note.findtext("Body/IndexModel/tomorrow"),
-                "uitrvTheDayAfterTomorrow" : note.findtext("Body/IndexModel/theDayAfterTomorrow")
+                "uitrvTomorrow_kor"        : "",
+                "uitrvTheDayAfterTomorrow" : note.findtext("Body/IndexModel/theDayAfterTomorrow"),
+                "uitrvTheDayAfterTomorrow_kor": ""
             }
             return content
 
@@ -107,7 +131,7 @@ def get_content_from_url(URL):
 # 메인 함수
 def main():
     # API 데이터 가져오기
-    print(NOW_DATETIME)
+    #print(NOW_DATETIME)
     TARGET_URL = TARGET_URL_ENDPOINT + TARGET_URL_SERVICEKEY + SERVICEKEY + TARGET_URL_AREANO + AREANO + TARGET_URL_TYPE + TYPE + TARGET_URL_TIME + NOW_DATETIME
     url_content = get_content_from_url(TARGET_URL)
     #print(url_content)
